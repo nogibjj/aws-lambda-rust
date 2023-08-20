6ec1ebd78dabbc48f85520fe5e3be04a32a6541e
Noah Gift <noah.gift@gmail.com>
2023-08-20 16:51:05 -0400

    comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rust AWS Lambda
 
+aws stepfunctions start-execution --state-machine-arn
+arn:aws:states:us-east-1:561744971673:stateMachine:RustDivideWorkflow --input '{ "x":1, "y":3}'
--- add-url/src/main.rs
+++ add-url/src/main.rs
@@ -1,35 +1,43 @@
 use lambda_http::{run, service_fn, Body, Error, Request, Response};
-use serde_json::json;
+use serde_json::json; // Import serde_json for handling JSON data
 
+// Define the asynchronous function handler for the Lambda function
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract the numbers from the URL path
+    // Extract the URL path from the incoming request
     let path = event.uri().path();
+    // Split the path into parts using '/' as the separator
     let parts: Vec<&str> = path.split('/').collect();
 
+    // Extract and parse the value of 'a' from the URL, default to 0 if parsing fails
     let a: i32 = parts.get(2).and_then(|s| s.parse().ok()).unwrap_or(0);
+    // Extract and parse the value of 'b' from the URL, default to 0 if parsing fails
     let b: i32 = parts.get(3).and_then(|s| s.parse().ok()).unwrap_or(0);
 
+    // Calculate the sum of 'a' and 'b'
     let sum = a + b;
 
-    // Create JSON payload with the sum
+    // Create a JSON payload containing the sum
     let payload = json!({ "total": sum });
 
-    // Return a response with the JSON payload
+    // Build the response with the JSON payload, setting the HTTP status to 200 and content type to application/json
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
         .body(Body::from(payload.to_string()))
         .map_err(Box::new)?;
+    // Return the response
     Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Initialize tracing for logging
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_target(false)
         .without_time()
         .init();
 
+    // Run the Lambda function using the defined handler
     run(service_fn(function_handler)).await
 }
